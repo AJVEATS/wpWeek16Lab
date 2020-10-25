aeecd1c9fec2bdf46bd063f1545628c5dda0826b
Alexander <40860289+AJVEATS@users.noreply.github.com>
2020-10-25 17:47:47 +0000

    Uploading completed lab task
    Uploading completed self study task 2: file upload demo from web programming week 16

--- selfStudyTask2.php
+++ selfStudyTask2.php
@@ -2,13 +2,24 @@
 <html>
     <head>
         <title>
-            
+            Self-Study Task 2: File upload demo
         </title>
     </head>
     <body>
-        <h1></h1>
+        <h1>Self-Study Task 2: File upload demo</h1>
+        <form action="selfStudyTask2.php" method="POST" enctype="multipart/form-data">
+            <label for="file">Select a file:</label>
+            <input type="file" name="uploaded"><br>
+            <input type="submit" name="filesubmit" value="Upload">
+        </form>
         <?php
-            
+            $dest = "./uploads/";
+            $dest_file = $dest.basename($_FILES['uploaded']['name']);
+            if (is_uploaded_file($_FILES['uploaded']['tmp_name'])) {
+                echo "The file".basename($_FILES['uploaded']['name'])." has been uploaded";
+            } else {
+                echo "Error, please try again!";
+            }
         ?>
     </body>    
 </html>
